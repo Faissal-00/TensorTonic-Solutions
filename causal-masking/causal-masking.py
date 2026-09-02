55bb1b4fbfd35494e41fa2173aa085072ede6dc3
Faissal Mouflla <132353836+Faissal-00@users.noreply.github.com>
2026-09-02 23:56:14 +0100

    Solve causal-masking

--- causal-masking/causal-masking.py
+++ causal-masking/causal-masking.py
@@ -5,9 +5,7 @@ def apply_causal_mask(scores: list, mask_value: float = -1e9) -> np.ndarray:
     Returns a causally masked NumPy array matching the shape of scores.
     """
     Matrix=np.array(scores)
-    #triangle_upper=np.triu(Matrix,k=1)
     dummy=np.ones_like(Matrix) 
     map_grid = np.triu(dummy, k=1)
-    future_mask = (map_grid == 1)
-    Output = np.where(future_mask, mask_value, Matrix)
+    Output = np.where(map_grid == 1, mask_value, Matrix)
     return Output
