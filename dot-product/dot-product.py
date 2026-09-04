d338e9e7f71bd82ac22b6b1a4296eafaed9f1814
Faissal Mouflla <132353836+Faissal-00@users.noreply.github.com>
2026-09-04 22:07:31 +0100

    Solve dot-product

--- dot-product/dot-product.py
+++ dot-product/dot-product.py
@@ -4,5 +4,7 @@ def dot_product(x: list, y: list) -> float:
     """
     Returns the dot product as a float.
     """
-    output = np.dot(np.array(x), np.array(y))
+    output = np.dot(np.array(x, dtype=float), np.array(y, dtype=float))
+    # OR
+    # output = np.array(x, dtype=float) @ np.array(y, dtype=float)
     return float(output)
