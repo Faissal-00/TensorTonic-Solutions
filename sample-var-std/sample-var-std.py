0ce3364fcc4ea581a267469a9ce0262212eb41bc
Faissal Mouflla <132353836+Faissal-00@users.noreply.github.com>
2026-09-04 20:10:50 +0100

    Solve sample-var-std

--- sample-var-std/sample-var-std.py
+++ sample-var-std/sample-var-std.py
@@ -10,3 +10,6 @@ def sample_var_std(x: list) -> dict:
     Dictio['variance'] = variance
     Dictio['standard_deviation'] = Std_deviation
     return Dictio
+
+    #ddof=0 (Population Variance): Divides the sum by exactly N. You use this when your dataset contains everything
+    #ddof=1 (Sample Variance): Divides the sum by N-1. This is called Bessel's Correction. You use this when your data is just a sample of a larger population
