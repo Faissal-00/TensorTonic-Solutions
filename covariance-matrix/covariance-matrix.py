1026dda26ebb5ebd9e7b0cfcab6f90c2e543f44b
Faissal Mouflla <132353836+Faissal-00@users.noreply.github.com>
2026-09-07 13:04:37 +0100

    Solve covariance-matrix

---
+++ covariance-matrix/covariance-matrix.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+def covariance_matrix(X: list) -> np.ndarray:
+    """
+    Returns the covariance matrix as a NumPy array.
+    """
+    N=len(X)
+    matrix = np.array(X).astype(float)
+    mean = np.mean(X, axis=0) 
+    matrix_c= X-mean
+    matrix_c_t=np.transpose(matrix_c)
+    covariance = (matrix_c_t@matrix) / (N-1)
+    return covariance
