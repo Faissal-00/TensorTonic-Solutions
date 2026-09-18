7cd280c37eab19e32b17e5e5244b2cfce2030d43
Faissal Mouflla <132353836+Faissal-00@users.noreply.github.com>
2026-09-18 02:44:38 +0100

    Solve zscore-standardization

--- zscore-standardization/zscore-standardization.py
+++ zscore-standardization/zscore-standardization.py
@@ -4,8 +4,9 @@ def zscore_standardize(X: list, axis: int = 0, eps: float = 1e-12) -> np.ndarray
     """
     Returns population Z-scores as a NumPy array matching the shape of X.
     """
+    X=np.array(X).astype(float)
     mean = np.mean(X, axis=axis, keepdims=True)
-    standard_deviation = np.std(X, axis=axis)
+    standard_deviation = np.std(X, axis=axis, keepdims=True)
     #np.where(condition, true_result, false_result)
     zscore = np.where(standard_deviation > eps, (X - mean) / standard_deviation, 0.0)
     return zscore
